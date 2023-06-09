00c479976107f5311c23609bec5c409679652930
Hamit <sadecehamit@gmail.com>
2023-06-09 18:23:53 +0200

    07-06-2023

---
+++ src/test/java/day02_practice/C01_DropDown.java
@@ -0,0 +1,52 @@
+package day02_practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public class C01_DropDown {
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void test1() {
+        // https://www.amazon.com/ sayfasina gidin
+        driver.get("https://www.amazon.com/");
+
+        // dropdown'dan "Books" secenegini secin
+        WebElement ddm=  driver.findElement(By.xpath("//*[@id='searchDropdownBox']"));
+        Select select = new Select(ddm);
+        select.selectByVisibleText("Books");
+
+
+        // arama cubuguna "Java" aratın
+        driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']")).sendKeys("Java",Keys.ENTER);
+
+        // arama sonuclarinin Java icerdigini test edin
+        WebElement javaIceriyormu = driver.findElement(By.xpath("(//*[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        Assert.assertTrue(javaIceriyormu.getText().contains("Java"));
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
